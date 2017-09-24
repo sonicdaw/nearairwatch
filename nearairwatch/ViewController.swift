cab6b60935878fd7d2ab1d288907410ba73140e9
sonicdaw <mshd@da.mbn.or.jp>
2017-09-25 07:10:18 +0900

    Initial Commit

---
+++ nearairwatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nearairwatch
+//
+//  Created by msum on 2017/09/25.
+//  Copyright © 2017年 entatonic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
