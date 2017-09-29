a05edb5b3e2183a3c89c54bff78bdc9a781d6a13
sonicdaw <mshd@da.mbn.or.jp>
2017-09-30 08:15:13 +0900

    Add displaying NearAir text on Apple Watch. The text is from NearAir server (+-0.3 latitude/longitude around Tokyo station).

--- nearairwatch WatchKit Extension/InterfaceController.swift
+++ nearairwatch WatchKit Extension/InterfaceController.swift
@@ -10,8 +10,11 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController,XMLParserDelegate {
     @IBOutlet var nearairText: WKInterfaceLabel!
+
+    var currentElement:String = ""
+    var parser = XMLParser()
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -23,7 +26,27 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
 
-        self.nearairText.setText("Hello NearAir")
+        let urlstr:String="https://entatonic.sakura.ne.jp/nearair/air.php?latitude=35.681382&longitude=139.766084"
+        let url: URL = URL(string: urlstr)!
+        
+        parser = XMLParser(contentsOf: url)!
+        parser.delegate = self
+
+        let success:Bool = parser.parse()
+
+        if success {
+        } else {
+        }
+    }
+
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        self.currentElement=elementName;
+    }
+
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if(self.currentElement=="text"){
+            self.nearairText.setText(string)
+        }
     }
     
     override func didDeactivate() {
