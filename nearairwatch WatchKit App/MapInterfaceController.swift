a6a1fbda44cfb342238a02c742e1d57d947bbf25
sonicdaw <mshd@da.mbn.or.jp>
2017-12-10 07:16:28 +0900

    Add MapInterfaceController

---
+++ nearairwatch WatchKit App/MapInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  MapInterfaceController.swift
+//  nearairwatch WatchKit App
+//
+//  Created by msum on 2017/12/09.
+//  Copyright © 2017年 entatonic. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class MapInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
